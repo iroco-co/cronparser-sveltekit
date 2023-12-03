3d38de966246ac8564815572ef1bf07960269c4b
Bruno Thomas <bruno@barreverte.fr>
2023-12-03 18:12:41 +0100

    feat: add adapter node

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-  import {parseExpression} from "cron-parser";
+  import CronParser from "cron-parser";
 
   let cronExpression = '*/2 * * * *'
-  let interval = parseExpression(cronExpression);
+  let interval = CronParser.parseExpression(cronExpression);
 </script>
 
 <h1>Welcome to SvelteKit + cron-parser + luxon</h1>
